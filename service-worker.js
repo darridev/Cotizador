e1db4cd47e89980f530804a43a7caa75d49e1383
David Arrieta <86837818+darridev@users.noreply.github.com>
2026-05-03 21:53:46 -0300

    Update service worker for caching and activation
    Refactor service worker to improve caching strategy and add activation event handling.

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,19 @@
 const CACHE_NAME = "chapas-app-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => {
+        if(k !== CACHE_NAME) return caches.delete(k);
+      }))
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
